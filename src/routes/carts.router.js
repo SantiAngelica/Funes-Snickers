6559a7ae8bf4039e0a482451b1e340341b246fe8
santiange <santiangelica410@gmail.com>
2024-07-23 17:59:11 -0300

    primer pre entrega terminada

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -1,54 +1,7 @@
 import { Router } from "express";
 const router = Router()
 import fs from "fs"
-
-class CartManager{
-    constructor(){
-        this.carts = []
-    }
-    async getCarts(){
-        try {
-            let res = await fs.promises.readFile("./src/assets/carts.json", "utf-8")
-            const carts = JSON.parse(res)
-            return carts
-        } catch (error) {
-            return this.carts
-        }
-    }
-    async newCart(){
-        const carts = await this.getCarts()
-        const newCart = {
-            id: carts.length + 1,
-            products: []
-        }
-        carts.push(newCart)
-        await fs.promises.writeFile("./src/assets/carts.json", JSON.stringify(carts, null, 2))
-    }
-    async getCartById(cid){
-        const carts = await this.getCarts()
-
-        const cart = carts.find(cart => cart.id == cid)
-        return cart
-    }
-    async addProdToCart(pid, cid){
-        const carts = await this.getCarts()
-        const cart = carts.find(cart => cart.id == cid)
-        if(cart){
-            const prod = cart.products.find(prod => prod.id == pid)
-            if(prod){
-                prod.quantity+=1
-            }else{
-                cart.products.push({
-                    id: pid,
-                    quantity: 1
-                })
-            }
-            await fs.promises.writeFile("./src/assets/carts.json", JSON.stringify(carts, null, 2))
-            return 1
-        }
-        return 0
-    }
-}
+import CartManager from "../manager/cart-manager.js";
 const manager = new CartManager()
 
 
@@ -57,7 +10,7 @@ const manager = new CartManager()
 //FUNCIONES PARA VERIFICAR QUE EXISTE UN PRODUCTO Y UN CARRITO CON SU RESPECTIVO ID
 async function getProducts(){
     try {
-        let res = await fs.promises.readFile("./src/assets/productos.json", "utf-8")
+        let res = await fs.promises.readFile("./src/assets/products.json", "utf-8")
         const productos = JSON.parse(res)
         return productos
     } catch (error) {
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -2,71 +2,7 @@ import { Router } from "express";
 const router = Router()
 import fs from "fs"
 
-
-class ProductManager {
-    constructor() {
-        this.products = []
-    }
-
-    async getProducts(){
-        try {
-            let res= await fs.promises.readFile("./src/assets/productos.json", "utf-8")
-            const products = JSON.parse(res)
-            return products
-        } catch (error) {
-            return this.products
-        }
-    }
-
-    async addProduct(newProduct) {
-        let products = await this.getProducts()
-        if (!newProduct.tittle || !newProduct.description || !newProduct.price || !newProduct.thumbnails ||!newProduct.code || !newProduct.stock || !newProduct.status || !newProduct.category ){
-
-            return 1
-        }
-        
-        if (products.some(prod => prod.code == newProduct.code)) {
-           
-            return 2
-        }
-
-
-        newProduct = {id: products.length+1, ...newProduct}
-
-        products.push(newProduct)
-        await fs.promises.writeFile("./src/assets/productos.json", JSON.stringify(products, null, 2))
-        return 3
-    }
-
-    async getProductById(id) {
-        const products = await this.getProducts()
-        let prod = products.find(prod => prod.id == id)
-        if (prod) {
-            return prod
-        }
-        else {
-            return null
-        }
-    }
-
-    async editProduct(id, newProperties){
-        const products = await this.getProducts()
-        let prod = products.find(prod => prod.id == id)
-        if(!prod)
-        {
-            return
-        }
-        newProperties.id && console.log("no se puede modificar el ID de un producto")
-        newProperties.tittle && (prod.tittle = newProperties.tittle)
-        newProperties.description && (prod.description = newProperties.description)
-        newProperties.price && (prod.price = newProperties.price)
-        newProperties.thumbnails && (prod.thumbnails = newProperties.thumbnails)
-        newProperties.code && (prod.code = newProperties.code)
-        newProperties.stock && (prod.stock = newProperties.stock)
-        newProperties.category && (prod.category = newProperties.category)
-        await fs.promises.writeFile("./src/assets/productos.json", JSON.stringify(products, null, 2))
-    }
-}
+import ProductManager from "../manager/product-manager.js";
 const manager = new ProductManager()
 
 
